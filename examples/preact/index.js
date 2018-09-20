8074b19736260dae5c92bcc65d905877c68349c2
yucopowo <yucopowo@gmail.com>
2018-09-20 22:19:50 +0800

    add morphdom example

--- examples/preact/index.js
+++ examples/preact/index.js
@@ -1,25 +1,20 @@
-const processor = (function () {
-    const unified = require('unified');
-    const parse = require('remark-parse');
-    const render = require('../../src/index');
-    const renderer = require('remark-preact-renderer');
-    const h = React.createElement;
-    return unified()
-        .use(parse, {})
-        .use(render, {
-            renderer: renderer,
-            h: h,
-            rootClassName: 'markdown-body',
-            rootTagName: 'main'
-        });
-})();
+const unified = require('unified');
+const parse = require('remark-parse');
+const render = require('../../src/index');
+const renderer = require('remark-preact-renderer');
+const processor = unified()
+    .use(parse, {})
+    .use(render, {
+        renderer: renderer,
+        h: preact.h,
+        rootClassName: 'markdown',
+        rootTagName: 'main'
+    });
 
 const file = processor.processSync(require('../md/maxiang.md'));
 const vdom = file.contents;
 
-const { h ,render } = preact;
-
-render(
-    h(vdom),
+preact.render(
+    vdom,
     document.getElementById('preview')
 );
